2593cf8ef32177b266d54008bc5afbda0d118630
Rodrigo Sanabria <rodrigosanabria22@gmail.com>
2020-07-02 20:52:56 +0200

    feat: encode + symbol for google services

--- src/engines/youtube.rs
+++ src/engines/youtube.rs
@@ -1,6 +1,12 @@
 use percent_encoding::{utf8_percent_encode, AsciiSet, CONTROLS};
 
-const FRAGMENT: &AsciiSet = &CONTROLS.add(b' ').add(b'"').add(b'<').add(b'>').add(b'`');
+const FRAGMENT: &AsciiSet = &CONTROLS
+    .add(b' ')
+    .add(b'"')
+    .add(b'<')
+    .add(b'>')
+    .add(b'`')
+    .add(b'+');
 
 fn construct_search_url(query: &str) -> String {
     let encoded_query = utf8_percent_encode(query, FRAGMENT).to_string();
